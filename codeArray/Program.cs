97f9433faa2a110d2ad0485acd8c1bae32ca4f19
Dell <ersadehmedov01@gmail.com>
2023-03-31 08:50:21 +0300

    Add project files.

---
+++ codeArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace codeArray
+{
+    internal class Program
+    {
+        //task1
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("ölçüsünü daxil edin: ");
+        //    int size = Convert.ToInt32(Console.ReadLine());
+        //    int[] array = new int[size];
+        //    int sum = 0;
+
+        //    Console.WriteLine("elementlerini daxil edin: ");
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        Console.Write("array[{0}] = ", array[i]);
+        //        sum += array[i];
+        //    }
+        //    Console.WriteLine("elementlerinin cemi: {0}", sum);
+        //}
+
+
+
+        //--------------------------------------------------------------------------------------------
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Cumle daxil edin: ");
+        //    string arr = Console.ReadLine();
+        //    IsUpper(arr);
+
+        //}
+
+        //static void IsUpper(string str)
+        //{
+        //    string arr1 = str.ToUpper();
+        //    string arr2 = str.ToUpper();
+        //    Console.ReadLine();
+        //}
+
+
+
+    }
+
+
+
+
+    
+}
